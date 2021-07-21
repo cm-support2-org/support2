2adc9700e672a0f4a13d9687bcf4d942e64ec835
CMSupport2 <82769965+cm-slu@users.noreply.github.com>
2021-07-21 19:36:35 +0200

    Update CMScriptControlUploadArchives.ps1

--- Scripts/CMScriptControlUploadArchives.ps1
+++ Scripts/CMScriptControlUploadArchives.ps1
@@ -6,7 +6,7 @@ Change Log
 #>
 $ExitCode = 0
 #Verification si le fichier existe. S'il existe pas creation
-$fileToMessage = "$env:TEMP\CMUploadArchives.txt"
+$fileToMessage = "$env:TEMP\CMCountLotCMD.txt"
 $dateLotCMD = ""
 
 if (Test-Path $fileToMessage -PathType leaf){
