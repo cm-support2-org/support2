164edc8d0387fb1f712a4a5a786b4ed4a9ff54d2
Seb <82769965+cm-slu@users.noreply.github.com>
2023-01-04 08:34:56 +0100

    Create omc_f_calculCheckDigit.sql

---
+++ Fonction/omc_f_calculCheckDigit.sql
@@ -0,0 +1,64 @@
+CREATE FUNCTION "omc"."omc_f_calculCheckDigit"(barcodes t_code_barre)
+RETURNS VARCHAR(13)
+DETERMINISTIC
+
+    //Cette fonction permet le calcul du checkdigit pour les EAN 8 ou 13.
+    //Cela permet de remplacer l'étoile de GOMC par le digit.
+
+BEGIN
+	DECLARE ls_barcodes t_code_barre;
+    DECLARE ls_digit char(1);
+
+    set ls_barcodes = barcodes;
+
+    //C'est un EAN13 ?
+    if len(barcodes) = 13 then
+
+        //On supprime l'étoile
+        set ls_barcodes = REPLACE(ls_barcodes,'*','');
+    
+    	set ls_digit = string(
+                                (10 - (CAST(SUBSTRING(ls_barcodes, 1, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 2, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 3, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 4, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 5, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 6, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 7, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 8, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 9, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 10, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 11, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 12, 1) AS INTEGER)
+                                    )%10
+                                )%10
+                             );
+        
+        //Contruction du codebarre avec le checkdigit
+        set ls_barcodes = string(ls_barcodes + ls_digit);
+
+    //C'est un EAN8 8
+    elseif len(barcodes) = 8 then
+        
+        //On supprime l'étoile
+        set ls_barcodes = REPLACE(ls_barcodes,'*','');
+    
+        set ls_digit = string(
+                              (
+                                10 - (3* CAST(SUBSTRING(ls_barcodes, 1, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 2, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 3, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 4, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 5, 1) AS INTEGER)
+                                + CAST(SUBSTRING(ls_barcodes, 6, 1) AS INTEGER)
+                                + 3* CAST(SUBSTRING(ls_barcodes, 7, 1) AS INTEGER)
+                                    )%10
+                              )%10
+                             );
+
+        //Contruction du codebarre avec le checkdigit
+        set ls_barcodes = string(ls_barcodes + ls_digit);
+    end if;
+
+	RETURN "ls_barcodes";
+END;
