6ceeb603394f1a81addcbd81b4e9bd50c1edcc86
Seb <82769965+cm-slu@users.noreply.github.com>
2023-06-26 14:45:05 +0200

    Create countJobInError.ps1

---
+++ Scripts/countJobInError.ps1
@@ -0,0 +1,99 @@
+<#
+.Description
+Compte le nombre de ticket present dans la base de donnes
+.NOTES
+Change Log
+#>
+$ExitCode = 0
+
+#Verification si le fichier existe. S'il existe pas creation
+$fileToMessage = "$env:TEMP\countJobInErreur.txt"
+
+if (Test-Path $fileToMessage -PathType leaf)
+{
+    #Si le fichier existe  
+}
+else
+{
+    #Creation du fichier
+    Set-Content $fileToMessage ""
+}
+
+Function ODBCConnection {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True,
+                    HelpMessage="DSN name of ODBC connection")]
+                    [string]$dsn,
+                    [string]$query,
+                    [bool]$lbvalide,
+                    [string]$Message,
+                    [string]$ResultQuery
+    )
+    
+    $conn = new-object system.data.odbc.odbcconnection
+    $conn.connectionstring = "DSN=$dsn"                      
+    
+    #Ouverture de l a connexion
+    $opened = $false
+    try {
+
+        $conn.Open()
+        
+        #Ouverture ok
+        $opened = $True
+    } catch {
+        
+        #Erreur
+        $errorMessage = $_.Exception.Message
+        $ExitCode = 99
+  
+    }
+    
+    #Execute query si connexion ok
+    If ($opened -eq $true){
+        
+        $cmd = New-object System.Data.Odbc.OdbcCommand($query,$conn)
+        $ds = New-Object system.Data.DataSet
+        (New-Object system.Data.odbc.odbcDataAdapter($cmd)).fill($ds) | out-null
+      
+        $ds.Tables[0]
+                
+        if ($ds.Tables[0].Rows.Count -gt 0){        
+            #$lbvalide = $false
+            
+            $ResultQuery = $ds.Tables[0]
+            #Ecrire le message dans un fichier text           
+            Add-Content $fileToMessage $Message 
+
+            foreach ($Row in $ds.Tables[0].Rows) { 
+                Add-Content $fileToMessage $Row.countTicket
+            }
+
+            #Ecrire message
+        Add-Content $fileToMessage $Message 
+        $ExitCode = 0
+        $lbvalide = $True   
+            
+        }else{       
+
+            #Ecrire message
+            Add-Content $fileToMessage $Message 
+            $ExitCode = 0
+            $lbvalide = $True                
+
+        }                   
+            
+        #Fermeture de la connexion avec la base
+        $conn.close()     
+
+        if ($lbvalide -eq $false){
+            $ExitCode = 1
+        }
+     
+    } 
+  
+    exit $ExitCode
+}
+
+ODBCConnection -dsn "CM_MOTEX_GOMC;Uid=dba;Pwd=sql" -query "SELECT count(*) FROM job"
