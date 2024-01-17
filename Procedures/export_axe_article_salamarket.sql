df810ef23414c9c002548240a0260e855d7f1918
Seb <82769965+cm-slu@users.noreply.github.com>
2024-01-17 11:55:44 +0100

    Update export_axe_article_salamarket.sql
    modification dans la requette principal dans la clause where article.art_chrono_m > article..art_derniere_modif

--- Procedures/export_axe_article_salamarket.sql
+++ Procedures/export_axe_article_salamarket.sql
@@ -330,7 +330,7 @@ BEGIN
         plu_sref.sra_id = sref_article.sra_id and
         plu_sref.sra_publisher = sref_article.sra_publisher and
         code_barre.codbr_codebarre is not null and
-        omc.article.art_chrono_m > dateArtModification            
+        article.art_derniere_modif > dateArtModification            
     Order by    
         article.art_id asc
  
